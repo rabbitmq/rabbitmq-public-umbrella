b765d19c7ec2c5c12268db094325a4138829621d
Tony Garnock-Jones <tonyg@lshift.net>
2008-07-03 13:41:22 +0100

    Initial commit, from repo-rebase-20080703121916_default (e96543d904a2)

---
+++ release.mk
@@ -0,0 +1,162 @@
+# This is a TEMPORARY umbrella makefile, that will likely not survive
+# the repo split.
+
+VERSION=0.0.0
+VDIR=v$(VERSION)
+
+SIGNING_KEY=056E8E56
+SIGNING_USER_EMAIL=info@rabbitmq.com
+SIGNING_USER_ID=RabbitMQ Release Signing Key <info@rabbitmq.com>
+
+PACKAGES_DIR=packages
+
+SERVER_PACKAGES_DIR=$(PACKAGES_DIR)/rabbitmq-server/$(VDIR)
+JAVA_CLIENT_PACKAGES_DIR=$(PACKAGES_DIR)/rabbitmq-java-client/$(VDIR)
+BUNDLES_PACKAGES_DIR=$(PACKAGES_DIR)/bundles/$(VDIR)
+
+.PHONY: packages
+
+all:
+	@echo Please choose a target from the Makefile.
+
+ifeq "$(UNOFFICIAL_RELEASE)$(GNUPG_PATH)" ""
+dist:
+	@echo "You must specify one of UNOFFICIAL_RELEASE (to true, if you don't want to sign packages) or GNUPG_PATH (to the location of the RabbitMQ keyring) when making dist."
+	@false
+else
+dist: packages bundles sign_everything
+endif
+
+prepare:
+	mkdir -p $(PACKAGES_DIR)
+	mkdir -p $(SERVER_PACKAGES_DIR)
+	mkdir -p $(JAVA_CLIENT_PACKAGES_DIR)
+	mkdir -p $(BUNDLES_PACKAGES_DIR)
+
+packages: prepare
+	$(MAKE) $(SERVER_PACKAGES_DIR)/rabbitmq-server-$(VERSION).tar.gz
+	$(MAKE) $(SERVER_PACKAGES_DIR)/rabbitmq-server-$(VERSION).zip
+	$(MAKE) $(SERVER_PACKAGES_DIR)/rabbitmq-server-generic-unix-$(VERSION).tar.gz
+	$(MAKE) $(SERVER_PACKAGES_DIR)/rabbitmq-server-windows-$(VERSION).zip
+	$(MAKE) debian_packages
+	$(MAKE) rpm_packages
+	$(MAKE) java_packages
+
+ifneq "$(UNOFFICIAL_RELEASE)" ""
+sign_everything:
+	true
+else
+sign_everything:
+	rpm --addsign \
+		--define '_signature gpg' \
+		--define '_gpg_path $(GNUPG_PATH)/.gnupg/' \
+		--define '_gpg_name $(SIGNING_USER_ID)' \
+		$(PACKAGES_DIR)/*/*/*.rpm
+	for p in \
+		$(SERVER_PACKAGES_DIR)/* \
+		$(JAVA_CLIENT_PACKAGES_DIR)/* \
+		$(BUNDLES_PACKAGES_DIR)/* \
+	; do \
+		[ -f $$p ] && \
+			HOME=$(GNUPG_PATH) gpg --default-key $(SIGNING_KEY) -abs -o $$p.asc $$p ; \
+	done
+endif
+
+bundles: packages
+	$(MAKE) windows_bundle
+
+$(SERVER_PACKAGES_DIR)/rabbitmq-server-$(VERSION).tar.gz: prepare rabbitmq-server
+	$(MAKE) -C rabbitmq-server clean srcdist VERSION=$(VERSION)
+	cp rabbitmq-server/dist/rabbitmq-server-*.tar.gz $(SERVER_PACKAGES_DIR)
+
+$(SERVER_PACKAGES_DIR)/rabbitmq-server-$(VERSION).zip: prepare rabbitmq-server
+	$(MAKE) -C rabbitmq-server clean srcdist VERSION=$(VERSION)
+	cp rabbitmq-server/dist/rabbitmq-server-*.zip $(SERVER_PACKAGES_DIR)
+
+$(SERVER_PACKAGES_DIR)/rabbitmq-server-generic-unix-$(VERSION).tar.gz: prepare rabbitmq-server
+	$(MAKE) -C rabbitmq-server/packaging/generic-unix clean dist VERSION=$(VERSION)
+	cp rabbitmq-server/packaging/generic-unix/rabbitmq-server-generic-unix-*.tar.gz $(SERVER_PACKAGES_DIR)
+
+$(SERVER_PACKAGES_DIR)/rabbitmq-server-windows-$(VERSION).zip: prepare rabbitmq-server
+	$(MAKE) -C rabbitmq-server/packaging/windows clean dist VERSION=$(VERSION)
+	cp rabbitmq-server/packaging/windows/rabbitmq-server-windows-*.zip $(SERVER_PACKAGES_DIR)
+
+debian_packages: prepare $(SERVER_PACKAGES_DIR)/rabbitmq-server-$(VERSION).tar.gz rabbitmq-server
+	$(MAKE) -C rabbitmq-server/packaging/debs/Debian clean package \
+		UNOFFICIAL_RELEASE=$(UNOFFICIAL_RELEASE) \
+		VERSION=$(VERSION) \
+		SIGNING_KEY_ID=$(SIGNING_KEY)
+	cp rabbitmq-server/packaging/debs/Debian/*$(VERSION)*.deb $(SERVER_PACKAGES_DIR)
+	$(MAKE) -C rabbitmq-server/packaging/debs/apt-repository all \
+		UNOFFICIAL_RELEASE=$(UNOFFICIAL_RELEASE) \
+		GNUPG_PATH=$(GNUPG_PATH) \
+		SIGNING_USER_EMAIL=$(SIGNING_USER_EMAIL)
+	cp -r rabbitmq-server/packaging/debs/apt-repository/debian $(PACKAGES_DIR)
+
+rpm_packages: prepare $(SERVER_PACKAGES_DIR)/rabbitmq-server-$(VERSION).tar.gz rabbitmq-server
+	$(MAKE) -C rabbitmq-server/packaging/RPMS/Fedora rpms VERSION=$(VERSION)
+	cp rabbitmq-server/packaging/RPMS/Fedora/RPMS/noarch/rabbitmq-server*.rpm $(SERVER_PACKAGES_DIR)
+
+java_packages: prepare rabbitmq-java-client
+	$(MAKE) -C rabbitmq-java-client clean dist VERSION=$(VERSION)
+	cp rabbitmq-java-client/build/*.tar.gz $(JAVA_CLIENT_PACKAGES_DIR)
+	cp rabbitmq-java-client/build/*.zip $(JAVA_CLIENT_PACKAGES_DIR)
+	cd $(JAVA_CLIENT_PACKAGES_DIR); unzip rabbitmq-java-client-javadoc-$(VERSION).zip
+
+WINDOWS_BUNDLE_TMP_DIR=$(PACKAGES_DIR)/complete-rabbitmq-bundle-$(VERSION)
+windows_bundle:
+	rm -rf $(WINDOWS_BUNDLE_TMP_DIR)
+	mkdir -p $(WINDOWS_BUNDLE_TMP_DIR)
+	[ -f /tmp/otp_win32_R11B-5.exe ] || \
+		wget -P /tmp http://www.erlang.org/download/otp_win32_R11B-5.exe
+	cp /tmp/otp_win32_R11B-5.exe $(WINDOWS_BUNDLE_TMP_DIR)
+	cp \
+		$(SERVER_PACKAGES_DIR)/rabbitmq-server-windows-$(VERSION).zip \
+		$(JAVA_CLIENT_PACKAGES_DIR)/rabbitmq-java-client-bin-$(VERSION).zip \
+		$(WINDOWS_BUNDLE_TMP_DIR)
+	cp ./README-windows-bundle $(WINDOWS_BUNDLE_TMP_DIR)/README
+	sed -i 's/%%VERSION%%/$(VERSION)/' $(WINDOWS_BUNDLE_TMP_DIR)/README
+	(cd $(WINDOWS_BUNDLE_TMP_DIR)/..; \
+		zip -r complete-rabbitmq-bundle-$(VERSION).zip complete-rabbitmq-bundle-$(VERSION);)
+	mv $(WINDOWS_BUNDLE_TMP_DIR)/../complete-rabbitmq-bundle-$(VERSION).zip \
+		$(BUNDLES_PACKAGES_DIR)
+	rm -rf $(WINDOWS_BUNDLE_TMP_DIR)
+
+clean:
+	rm -rf $(PACKAGES_DIR)
+	$(MAKE) -C rabbitmq-server clean
+	$(MAKE) -C rabbitmq-server/packaging/generic-unix clean
+	$(MAKE) -C rabbitmq-server/packaging/windows clean
+	$(MAKE) -C rabbitmq-server/packaging/debs/Debian clean
+	$(MAKE) -C rabbitmq-server/packaging/debs/apt-repository clean
+	$(MAKE) -C rabbitmq-server/packaging/RPMS/Fedora clean
+	$(MAKE) -C rabbitmq-java-client clean
+
+###########################################################################
+
+RSYNC_CMD=rsync -irvpl
+DEPLOY_HOST=charlotte
+
+DEPLOY_RSYNC_CMDS=\
+	set -x; \
+	for subdirectory in rabbitmq-server rabbitmq-java-client bundles; do \
+		ssh $(DEPLOY_HOST) "(cd $${DEPLOY_ROOT}/releases; mkdir -p $$subdirectory; chmod g+w $$subdirectory)"; \
+		$(RSYNC_CMD) --delete-after $(PACKAGES_DIR)/$$subdirectory/* \
+			$(DEPLOY_HOST):$${DEPLOY_ROOT}/releases/$$subdirectory ; \
+	done; \
+	$(RSYNC_CMD) --delete-after \
+			$(PACKAGES_DIR)/debian \
+		$(DEPLOY_HOST):$${DEPLOY_ROOT}/releases; \
+	UNPACKED_JAVADOC_DIR=`(cd packages/rabbitmq-java-client; ls -td */rabbitmq-java-client-javadoc-*/ | head -1)`; \
+	ssh $(DEPLOY_HOST) "(cd $${DEPLOY_ROOT}/releases/rabbitmq-java-client; rm -f current-javadoc; ln -s $${UNPACKED_JAVADOC_DIR} current-javadoc)"; \
+	set +x
+
+deploy-stage: fixup-permissions-for-deploy
+	(DEPLOY_ROOT=/home/rabbitmq/stage-extras; $(DEPLOY_RSYNC_CMDS))
+
+deploy-live: fixup-permissions-for-deploy
+	(DEPLOY_ROOT=/home/rabbitmq/live-extras; $(DEPLOY_RSYNC_CMDS))
+
+fixup-permissions-for-deploy:
+	chmod -R g+w $(PACKAGES_DIR)
+	chmod g+s `find $(PACKAGES_DIR) -type d`
