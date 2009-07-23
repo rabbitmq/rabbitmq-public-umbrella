3e3d4b2a6a3b95a5b68ac1d90299142b30e84578
Paul Jones <pauljones23@gmail.com>
2009-07-23 16:15:44 +0100

    Added a bunch more content to the include.mk file

--- include.mk
+++ include.mk
@@ -1,14 +1,46 @@
-# This is a global include file for all Makefiles
+# This is a global include file for all Makefiles. It is expected that modules
+# will include it with a command similar to "include ../include.mk". Before
+# inclusion, the following variables MUST be set:
+#  PACKAGE=    -- The name of the package
+# 
+# The following optional variables can be set if your build requires it:
+#  GENERATED_SOURCES	-- The names of modules that are automatically generated.
+#			   Note that the names provided should EXCLUDE the .erl extension 
 
 EBIN_DIR=ebin
 SOURCE_DIR=src
 INCLUDE_DIR=include
 DIST_DIR=dist
 
-INCLUDES=$(wildcard $(INCLUDE_DIR)/*.hrl)
-SOURCES=$(wildcard $(SOURCE_DIR)/*.erl)
-TARGETS=$(patsubst $(SOURCE_DIR)/%.erl, $(EBIN_DIR)/%.beam, $(SOURCES))
+SHELL=/bin/bash
+ERLC=erlc
+ERL=erl
 
-ERLC_OPTS=-I $(INCLUDE_DIR) -o $(EBIN_DIR) -Wall
+INCLUDE_OPTS=-I $(INCLUDE_DIR) $(foreach DEP, $(DEPS), -I ../$(DEP)/include)
+SOURCES=$(wildcard $(SOURCE_DIR)/*.erl)
+TARGETS=$(foreach GEN, $(GENERATED_SOURCES), src/$(GEN).erl)  \
+        $(patsubst $(SOURCE_DIR)/%.erl, $(EBIN_DIR)/%.beam, $(SOURCES)) \
+        $(foreach GEN, $(GENERATED_SOURCES), ebin/$(GEN).beam)
+
+ERLC_OPTS=$(INCLUDE_OPTS) -o $(EBIN_DIR) -Wall
 
 RABBIT_SERVER=rabbitmq-server
+
+all: $(TARGETS)
+
+diag:
+	echo $(INCLUDE_OPTS)
+
+$(EBIN_DIR)/%.beam: $(SOURCE_DIR)/%.erl
+	$(ERLC) $(ERLC_OPTS) -pa $(EBIN_DIR) $<
+
+package: clean all
+	rm -rf $(DIST_DIR)
+	mkdir -p $(DIST_DIR)/$(PACKAGE)
+	cp -r $(EBIN_DIR) $(DIST_DIR)/$(PACKAGE)
+	(cd $(DIST_DIR); zip -r $(PACKAGE).ez $(PACKAGE))
+
+clean:
+	rm -f $(EBIN_DIR)/*.beam
+	rm -f erl_crash.dump
+	$(foreach GEN, $(GENERATED_SOURCES), rm -f src/$(GEN);)
