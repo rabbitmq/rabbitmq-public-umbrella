4196729d48cb97198d9841c5fa11e82e06aaad1c
Paul Jones <paulj@lshift.net>
2009-08-04 10:07:45 +0100

    Added support for internal dependencies

--- include.mk
+++ include.mk
@@ -5,6 +5,7 @@
 # 
 # The following optional variables can be set if your build requires it:
 #  DEPS                 -- Other projects that your build depends on (eg rabbitmq-server)
+#  INTERNAL_DEPS        -- Internal dependencies that need to be built and included.
 #  GENERATED_SOURCES	-- The names of modules that are automatically generated.
 #			   Note that the names provided should EXCLUDE the .erl extension 
 #  EXTRA_PACKAGE_DIRS   -- The names of extra directories (over ebin) that should be included
@@ -19,6 +20,7 @@ SOURCE_DIR=src
 TEST_DIR=test
 INCLUDE_DIR=include
 DIST_DIR=dist
+DEPS_DIR=deps
 
 SHELL=/bin/bash
 ERLC=erlc
@@ -27,7 +29,8 @@ ERL=erl
 INCLUDE_OPTS=-I $(INCLUDE_DIR) $(foreach DEP, $(DEPS), -I ../$(DEP)/include)
 SOURCES=$(wildcard $(SOURCE_DIR)/*.erl)
 TEST_SOURCES=$(wildcard $(TEST_DIR)/*.erl)
-TARGETS=$(foreach GEN, $(GENERATED_SOURCES), src/$(GEN).erl)  \
+TARGETS=$(foreach DEP, $(INTERNAL_DEPS), $(DEPS_DIR)/$(DEP)/ebin) \
+	$(foreach GEN, $(GENERATED_SOURCES), src/$(GEN).erl)  \
         $(patsubst $(SOURCE_DIR)/%.erl, $(EBIN_DIR)/%.beam, $(SOURCES)) \
         $(foreach GEN, $(GENERATED_SOURCES), ebin/$(GEN).beam)
 TEST_TARGETS=$(patsubst $(TEST_DIR)/%.erl, $(TEST_EBIN_DIR)/%.beam, $(TEST_SOURCES))
@@ -35,7 +38,8 @@ TEST_TARGETS=$(patsubst $(TEST_DIR)/%.erl, $(TEST_EBIN_DIR)/%.beam, $(TEST_SOURC
 ERLC_OPTS=$(INCLUDE_OPTS) -o $(EBIN_DIR) -Wall
 TEST_ERLC_OPTS=$(INCLUDE_OPTS) -o $(TEST_EBIN_DIR) -Wall
 
-TEST_LOAD_PATH=-pa $(EBIN_DIR) -pa $(TEST_EBIN_DIR) $(foreach DEP, $(DEPS), -pa ../$(DEP)/ebin)
+TEST_LOAD_PATH=-pa $(EBIN_DIR) -pa $(TEST_EBIN_DIR) $(foreach DEP, $(DEPS), -pa ../$(DEP)/ebin) \
+	$(foreach DEP, $(INTERNAL_DEPS), -pa $(DEPS_DIR)/$(DEP)/ebin)
 
 LOG_BASE=/tmp
 LOG_IN_FILE=true
@@ -53,12 +57,22 @@ all: $(TARGETS)
 diag:
 	echo $(INCLUDE_OPTS)
 
+$(EBIN_DIR):
+	mkdir -p $(EBIN_DIR)
+
 $(EBIN_DIR)/%.beam: $(SOURCE_DIR)/%.erl
+	@mkdir -p $(EBIN_DIR)
 	$(ERLC) $(ERLC_OPTS) -pa $(EBIN_DIR) $<
+
+$(TEST_EBIN_DIR):
+	mkdir -p $(TEST_EBIN_DIR)
 
 $(TEST_EBIN_DIR)/%.beam: $(TEST_DIR)/%.erl
 	@mkdir -p $(TEST_EBIN_DIR)
 	$(ERLC) $(TEST_ERLC_OPTS) -pa $(TEST_EBIN_DIR) $<
+
+$(DEPS_DIR)/%/ebin:
+	make -C $(shell dirname $@)
 
 package: clean all
 	rm -rf $(DIST_DIR)
@@ -75,3 +89,4 @@ clean:
 	rm -f $(TEST_EBIN_DIR)/*.beam
 	rm -f erl_crash.dump
 	$(foreach GEN, $(GENERATED_SOURCES), rm -f src/$(GEN);)
+	$(foreach DEP, $(INTERNAL_DEPS), make -C $(DEPS_DIR)/$(DEP) clean)
