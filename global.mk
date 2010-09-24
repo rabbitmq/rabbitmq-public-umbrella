1ce79c85c839f47935dd4c548de3d8f7a1d9bceb
Matthew Sackman <matthew@rabbitmq.com>
2010-09-24 18:32:15 +0100

    Now that we properly support erlang-rfc4627, we don't need it to be non-integrated

--- global.mk
+++ global.mk
@@ -11,16 +11,13 @@ export ERLC_OPTS ?= -Wall +debug_info
 export DIST_DIR ?= dist
 export DEPS_DIR ?= deps
 
-NON_INTEGRATED:=rabbitmq-server rabbitmq-erlang-client erlang-rfc4627
+NON_INTEGRATED:=rabbitmq-server rabbitmq-erlang-client
 
 $(abspath $(CURDIR)/../rabbitmq-erlang-client)_OUTPUT_EZS:=amqp_client rabbit_common
 $(abspath $(CURDIR)/../rabbitmq-erlang-client)_VERSION:=$(GLOBAL_VERSION)
 
 $(abspath $(CURDIR)/../rabbitmq-server)_OUTPUT_EZS:=
 $(abspath $(CURDIR)/../rabbitmq-server)_VERSION:=$(GLOBAL_VERSION)
-
-$(abspath $(CURDIR)/../erlang-rfc4627)_OUTPUT_EZS:=rfc4627_jsonrpc
-$(abspath $(CURDIR)/../erlang-rfc4627)_VERSION:=$(GLOBAL_VERSION)
 
 ifeq "$(MAKECMDGOALS)" ""
 TESTABLEGOALS:=$(.DEFAULT_GOAL)
