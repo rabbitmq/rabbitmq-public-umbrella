8c022dac3a47d01c032310f5335a5b45fb5554f8
Matthew Sackman <matthew@rabbitmq.com>
2010-09-29 13:52:35 +0100

    Support run_in_broker and associated refactorings

--- top.mk
+++ top.mk
@@ -7,18 +7,31 @@ run_TEST_EBIN_DIR:=$($(PACKAGE_DIR)_TEST_EBIN_DIR)
 run: $($(PACKAGE_DIR)_TEST_EBIN_BEAMS) $(PACKAGE_DIR)_OUTPUT_EZS
 	ERL_LIBS=$($@_DIST_DIR) $(ERL) -pa $($@_TEST_EBIN_DIR)
 
+# $(1) is $(PACKAGE_DIR)
+# $(2) is extra SERVER_START_ARGS
+# $(3) is extra RABBITMQ_* env vars
+define prepare_and_boot_broker
+	rm -rf $(1)/tmp $(1)/plugins $(1)/cover
+	mkdir -p $(1)/tmp $(1)/plugins
+	cp -a $(1)/$(DIST_DIR)/*.ez $(1)/plugins
+	rm -f $(1)/plugins/rabbit_common*
+	RABBITMQ_PLUGINS_DIR=$(1)/plugins RABBITMQ_NODENAME=$(NODENAME) \
+	  RABBITMQ_LOG_BASE=$(1)/tmp RABBITMQ_MNESIA_BASE=$(1)/tmp \
+	  RABBITMQ_SERVER_START_ARGS="-pa $($(1)_TEST_EBIN_DIR) $(2)" \
+	  $(3) $($@_DIR)/../rabbitmq-server/scripts/rabbitmq-server
+endef
+
+.PHONY: run_in_broker
+run_in_broker_DIR:=$(PACKAGE_DIR)
+run_in_broker: $($(PACKAGE_DIR)_TEST_EBIN_BEAMS) $(PACKAGE_DIR)_OUTPUT_EZS
+	$(call prepare_and_boot_broker,$($@_DIR),,RABBITMQ_ALLOW_INPUT=true)
+
 .PHONY: test
 test_DIR:=$(PACKAGE_DIR)
 test_TEST_EBIN_DIR:=$($(PACKAGE_DIR)_TEST_EBIN_DIR)
 test: $($(PACKAGE_DIR)_TEST_EBIN_BEAMS)
-	rm -rf $($@_DIR)/tmp $($@_DIR)/plugins $($@_DIR)/cover
-	mkdir -p $($@_DIR)/tmp $($@_DIR)/plugins
-	cp -a $($@_DIR)/$(DIST_DIR)/*.ez $($@_DIR)/plugins
-	rm -f $($@_DIR)/plugins/rabbit_common*
-	RABBITMQ_PLUGINS_DIR=$($@_DIR)/plugins RABBITMQ_NODENAME=$(NODENAME) \
-	  RABBITMQ_LOG_BASE=$($@_DIR)/tmp RABBITMQ_MNESIA_BASE=$($@_DIR)/tmp \
-	  RABBITMQ_SERVER_START_ARGS="-pa $($($@_DIR)_TEST_EBIN_DIR) -coverage directories [$($@_COVERAGE)]" \
-	  $($@_DIR)/../rabbitmq-server/scripts/rabbitmq-server & sleep 8
+	$(call prepare_and_boot_broker,$($@_DIR),-coverage directories [$($@_COVERAGE)],) &
+	sleep 8
 	@echo > $($@_DIR)/rabbit-test-output && \
 	{ $(foreach BOOT_CMD,$(BOOT_CMDS),\
             echo "$(BOOT_CMD)." | tee -a $($@_DIR)/rabbit-test-output | $(ERL_CALL) $(ERL_CALL_OPTS) | tee -a $($@_DIR)/rabbit-test-output | egrep "{ok, " >/dev/null && ) true && \
