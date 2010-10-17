604af9ee71dfb81516e4cd04e768cce4b0ad58e9
Matthew Sackman <matthew@rabbitmq.com>
2010-10-17 23:22:46 +0100

    Record when we recurse into a package, and detect duplicates. Have to protect DEPS properly now so it just becomes a normal variable, lifted to the package namespace like all the others. Remove the previously unused PACKAGE_DIR_DEPS (which contained abspaths to the deps)

--- deps.mk
+++ deps.mk
@@ -22,29 +22,29 @@ define package_deps
 # Set up a pointer from the child back to its parents
 $(1)_PARENTS:=$(PACKAGE_DIR) $($(1)_PARENTS)
 #$$(info $(1) parents are $$($(1)_PARENTS))
-
-# Append the current child to the list of deps for the parent
-$(PACKAGE_DIR)_DEPS:=$(1) $($(PACKAGE_DIR)_DEPS)
-#$$(info $(PACKAGE_DIR)_DEPS = $$($(PACKAGE_DIR)_DEPS))
 endef
 
 define package_recurse
 # child abspath is in $(1). Child name is in $(2)
 PACKAGE_DIR:=$(1)
 PACKAGE_NAME:=$(2)
+ifdef $(strip $(1))_VISITED
+include ../common.mk
+else
+$(strip $(1))_VISITED:=true
 ifeq "$(filter $(notdir $(strip $(1))),$(NON_INTEGRATED))" ""
 $(foreach VAR,$(VARS),$(eval $(VAR):=undefined))
-DEPS:=
 include $(strip $(1))/Makefile
 else
 include ../non-integrated.mk
+endif
 endif
 endef
 
 ifdef BASE_CASE_DONE
 include ../targets.mk
-$(foreach DEP,$(DEPS),$(eval $(call package_deps,$(abspath $(CURDIR)/../$(strip $(DEP))))))
-$(foreach DEP,$(DEPS),$(eval $(call package_recurse,$(abspath $(CURDIR)/../$(strip $(DEP))),$(strip $(DEP)))))
+$(foreach DEP,$($(PACKAGE_DIR)_DEPS),$(eval $(call package_deps,$(abspath $(CURDIR)/../$(strip $(DEP))))))
+$(foreach DEP,$($(PACKAGE_DIR)_DEPS),$(eval $(call package_recurse,$(abspath $(CURDIR)/../$(strip $(DEP))),$(strip $(DEP)))))
 else
 .DEFAULT_GOAL:=$(CURDIR)_OUTPUT_EZS_PATHS
 
