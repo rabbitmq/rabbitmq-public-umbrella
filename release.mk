be2bab6770eacd4e54905024479d4987123b4220
Alvaro Videla <alvaro@rabbitmq.com>
2012-11-16 13:44:19 +0100

    renamed release package to standalone

--- release.mk
+++ release.mk
@@ -159,10 +159,10 @@ rabbitmq-server-generic-unix-packaging: rabbitmq-server-srcdist
 	$(MAKE) -C rabbitmq-server/packaging/generic-unix dist VERSION=$(VERSION)
 	cp rabbitmq-server/packaging/generic-unix/rabbitmq-server-generic-unix-*.tar.gz $(SERVER_PACKAGES_DIR)
 
-.PHONY: rabbitmq-server-mac-standalone-packaging
-rabbitmq-server-mac-standalone-packaging: rabbitmq-server-srcdist
-	$(MAKE) -C rabbitmq-server/packaging/mac dist VERSION=$(VERSION)
-	cp rabbitmq-server/packaging/mac/rabbitmq-server-mac-standalone-*.tar.gz $(SERVER_PACKAGES_DIR)
+.PHONY: rabbitmq-server-standalone-packaging
+rabbitmq-server-standalone-packaging: rabbitmq-server-srcdist
+	$(MAKE) -C rabbitmq-server/packaging/standalone dist VERSION=$(VERSION)
+	cp rabbitmq-server/packaging/standalone/rabbitmq-server-standalone-*.tar.gz $(SERVER_PACKAGES_DIR)
 
 .PHONY: rabbitmq-server-windows-packaging
 rabbitmq-server-windows-packaging: rabbitmq-server-srcdist
