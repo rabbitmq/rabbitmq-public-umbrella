55ae2ba0d925d330c07a529f2b86aaae89e769e9
Tim Watson <tim@rabbitmq.com>
2013-05-16 14:28:29 +0100

    tweak install-otp script to use working directory beneath HOME

--- release-build/install-otp.sh
+++ release-build/install-otp.sh
@@ -9,18 +9,19 @@ version=$1
 
 if [ ! -f $HOME/otp-$version/.ok ] ; then
     rm -rf $HOME/otp-$version/.ok
-    cd /var/tmp
+    mkdir -p $HOME/tmp-otp-build
+    cd $HOME/tmp-otp-build
     [ -f otp_src_$version.tar.gz ] || wget http://erlang.org/download/otp_src_$version.tar.gz
     rm -rf otp_src_$version
     tar xzf otp_src_$version.tar.gz
     cd otp_src_$version
-    { ./configure --prefix=$HOME/otp-$version 2>&1 && touch .configure-ok ; } | tee /var/tmp/otp-$version.log
+    { ./configure --prefix=$HOME/otp-$version 2>&1 && touch .configure-ok ; } | tee $HOME/tmp-otp-build/otp-$version.log
     test -e .configure-ok
-    { make 2>&1 && touch .make-ok ; } | tee -a /var/tmp/otp-$version.log
+    { make 2>&1 && touch .make-ok ; } | tee -a $HOME/tmp-otp-build/otp-$version.log
     test -e .make-ok
-    { make install 2>&1 && touch .make-install-ok ; } | tee -a /var/tmp/otp-$version.log
+    { make install 2>&1 && touch .make-install-ok ; } | tee -a $HOME/tmp-otp-build/otp-$version.log
     test -e .make-install-ok
-    cd /var/tmp
+    cd $HOME/tmp-otp-build
     rm -rf otp_src_$version
     touch $HOME/otp-$version/.ok
 fi
