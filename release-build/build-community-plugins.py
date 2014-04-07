7574241e7bc2c781b8ec6e520e0394285ada8e30
Simon MacMullen <simon@rabbitmq.com>
2014-04-07 16:15:44 +0100

    Build jsonrpc and friends, build in defined order, actually implement --plugin-tag, support wrapper plugins which already have a git version.

--- release-build/build-community-plugins.py
+++ release-build/build-community-plugins.py
@@ -3,16 +3,17 @@
 from optparse import OptionParser
 import sys, subprocess, os, copy
 
-PLUGINS = \
-    {'rabbit_udp_exchange':
-         {'url': 'https://github.com/tonyg/udp-exchange'},
-     'rabbit_presence_exchange' :
-         {'url': 'https://github.com/tonyg/presence-exchange'},
-     'rabbitmq_auth_backend_http' :
-         {'url': 'https://github.com/simonmacmullen/rabbitmq-auth-backend-http'},
-     'rabbitmq_lvc' :
-         {'url': 'https://github.com/simonmacmullen/rabbitmq-lvc-plugin'}
-     }
+# There is no real dependency management here, if there are
+# dependencies between community plugins list them in order.
+PLUGINS = [
+    ('rfc4627_jsonrpc',                   {'url': 'https://github.com/rabbitmq/erlang-rfc4627-wrapper',
+                                          'version-add-hash': False}),
+    ('rabbitmq_jsonrpc',                  {'url': 'https://github.com/rabbitmq/rabbitmq-jsonrpc'}),
+    ('rabbitmq_jsonrpc_channel',          {'url': 'https://github.com/rabbitmq/rabbitmq-jsonrpc-channel'}),
+    ('rabbitmq_jsonrpc_channel_examples', {'url': 'https://github.com/rabbitmq/rabbitmq-jsonrpc-channel-examples'}),
+    ('rabbitmq_auth_backend_http',        {'url': 'https://github.com/simonmacmullen/rabbitmq-auth-backend-http'}),
+    ('rabbitmq_lvc',                      {'url': 'https://github.com/simonmacmullen/rabbitmq-lvc-plugin'})
+]
 
 OTP_VERSION="R13B03"
 BUILD_DIR = "/var/tmp/plugins-build/"
@@ -36,11 +37,10 @@ def main():
                       help="clone from alternative hg repository base URL")
     (options, args) = parser.parse_args()
     if options.plugin is None:
-        plugins = PLUGINS.keys()
+        plugins = PLUGINS
     else:
-        if options.plugin in PLUGINS:
-            plugins = [options.plugin]
-        else:
+        plugins = [(k, v) for (k, v) in PLUGINS if k == options.plugin]
+        if len(plugins) == 0:
             print "Plugin {0} not found".format(options.plugin)
             sys.exit(1)
     if options.repo_base is not None:
@@ -54,7 +54,7 @@ def main():
     ensure_otp()
     checkout(options.server_tag)
     print "Building..."
-    [build(p) for p in plugins]
+    [build(p, options.plugin_tag) for p in plugins]
 
 def ensure_dir(d):
     if not os.path.exists(d):
@@ -96,24 +96,42 @@ def get_tag(lines):
 def server_version():
     return RABBITMQ_TAG[10:].replace('_', '.')
 
-def build(plugin):
+def build((plugin, details), tag):
     print " * {0}".format(plugin)
     cd(BUILD_DIR + "/rabbitmq-public-umbrella")
-    details = PLUGINS[plugin]
     url = details['url']
+    if 'version-add-hash' in details:
+        version_add_hash = details['version-add-hash']
+    else:
+        version_add_hash = True
     do("git", "clone", "-q", url)
     checkout_dir = url.split("/")[-1].split(".")[0]
     hash = do("git", "--git-dir={0}/.git".format(checkout_dir),
               "rev-parse", "HEAD")[0:8]
     plugin_version = "{0}-{1}".format(server_version(), hash)
     cd(CURRENT_DIR + "/" + checkout_dir)
+    if tag is None:
+        do("git", "checkout", "master")
+    else:
+        do("git", "checkout", tag)
+    hash = do("git", "--git-dir=./.git", "rev-parse", "HEAD")[0:8]
+    if version_add_hash:
+        plugin_version = "{0}-{1}".format(server_version(), hash)
+    else:
+        plugin_version = server_version()
     do("make", "-j", "VERSION={0}".format(plugin_version), "srcdist")
     do("make", "-j", "VERSION={0}".format(plugin_version), "dist")
-    do("cp", "{0}/srcdist/{1}-{2}-src.tar.bz2".format(CURRENT_DIR, plugin,
-                                                      plugin_version),
+    do("cp",
+       find_package("{0}/srcdist/".format(CURRENT_DIR), plugin, ".tar.bz2"),
        "{0}/plugins".format(BUILD_DIR))
-    do("cp", "{0}/dist/{1}-{2}.ez".format(CURRENT_DIR, plugin, plugin_version),
+    do("cp", find_package("{0}/dist/".format(CURRENT_DIR), plugin, ".ez"),
        "{0}/plugins".format(BUILD_DIR))
+
+def find_package(dir, prefix, suffix):
+    for f in os.listdir(dir):
+        if f.startswith(prefix) and f.endswith(suffix):
+            return os.path.join(dir, f)
+    raise ('no_package', dir, prefix, suffix)
 
 def do(*args):
     path = os.environ['PATH']
